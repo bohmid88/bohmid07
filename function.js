68b695b9abb753d118c0b7097df30894fb06b549
bohmid88 <mazhoudahmed127@gmail.com>
2025-03-24 10:22:57 +0100

    Add files via upload

--- function.js
+++ function.js
@@ -1,19 +1,10 @@
 function copy(ch) {
-  // Get the text you want to copy
   var textToCopy = document.getElementById(ch).value;
-
-  // Create a temporary input element to hold the text
   var tempInput = document.createElement("input");
   document.body.appendChild(tempInput);
   tempInput.value = textToCopy;
-
-  // Select and copy the text
   tempInput.select();
   document.execCommand("copy");
-
-  // Remove the temporary input element
   document.body.removeChild(tempInput);
-
-  // Optionally, alert the user that the text has been copied
   alert("link copied to clipboard!");
 }
